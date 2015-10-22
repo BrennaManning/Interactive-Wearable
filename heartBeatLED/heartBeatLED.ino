8b60125dc38569a6883f901644bddbdba13f6926
David Abrahams <david.abrahams@students.olin.edu>
2015-10-22 00:05:21 -0400

    moving avg and clean code

--- heartBeatLED/heartBeatLED.ino
+++ heartBeatLED/heartBeatLED.ino
@@ -1,50 +1,75 @@
-unsigned long time;
-byte sleepTime = 40;
+unsigned long startTime;
+
+byte num_seconds = 4;
+int sleepTime = 50; // ms, read 20 times per second
+
+const int numReadings = 80; // TODO: THIS SHOULDNT BE HARDCODED
 
 int inPin = A0;    // select the input pin for the potentiometer
-int ledPin1 = 12;// select the pin for the LED
-int ledPin2 = 11;// select pin for second LED
-int val = 0;
-float voltage = 0.00;  // variable to store the value coming from the sensor
+int ledPin = 12;  // select the pin for the LED
+
+int voltage; // variable to store read-in
+
+int readings[numReadings];
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;
 
 void setup() {
   // declare the ledPin as an OUTPUT:
-  pinMode(ledPin1, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+
+  // initialize all the readings to 0:
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+
   Serial.begin(9600);
 }
 
 void loop() {
 
-  time = millis();
-  // read the value from the sensor:
-  val = analogRead(inPin);
-  float voltage = map(val, 0, 1023, 0, 5000);
-  // turn the ledPin on
-  voltage = voltage/1000;
-  Serial.println(voltage);
+  // get the start time
+  startTime = millis();
 
-  // TODO: IMPLEMENT MOVING AVERAGE CODE AND UPDATE DIGITAL WRITES TO CORRECT
-  // PINS
-  float heartbeat0 = 2;
-  float heartbeat1 = 1;
+  // subtract the last reading:
+  total = total - readings[readIndex];
+
+  // read the value from the sensor and map to mV
+  voltage = analogRead(inPin);
+  voltage = map(voltage, 0, 1023, 0, 5000);
+
+  //
+  readings[readIndex] = voltage;
+  total = total + voltage;
+  readIndex = readIndex + 1;
+    // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  average = total / numReadings;
+  Serial.print("Current reading: ");
+  Serial.print(voltage);
+  Serial.print("...Average: ");
+  Serial.println(average);
+
+  float beat_cuttoff = average;
 
   //When should lights turn on:
-  if (voltage > heartbeat0)
+  if (voltage > beat_cuttoff)
   {
-  digitalWrite(ledPin1, HIGH);
+  digitalWrite(ledPin, HIGH);
   }
   else
   {
-    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin, LOW);
   }
-  if (voltage > heartbeat1)
-  {
-    digitalWrite(ledPin2, HIGH);
-  }
-  else
-  {
-    digitalWrite(ledPin2, LOW);
-  }
-  delay(sleepTime - (millis() - time));
+
+  // the amount of time this loop cycle took to run
+  int runTime = (millis() - startTime);
+
+  // take the runtime into account
+  delay(max(sleepTime - runTime, 0));
 }
