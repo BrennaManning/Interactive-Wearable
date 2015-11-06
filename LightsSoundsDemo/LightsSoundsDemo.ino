5500f61b422ddee86859e57d282d78dbd6b0541e
David Abrahams <david.abrahams@students.olin.edu>
2015-11-06 00:55:56 -0500

    vibration code

--- LightsSoundsDemo/LightsSoundsDemo.ino
+++ LightsSoundsDemo/LightsSoundsDemo.ino
@@ -14,7 +14,7 @@ Adafruit_DCMotor *myMotor2 = AFMS.getMotor(3);
 Adafruit_DCMotor *myMotor3 = AFMS.getMotor(4);
 
 // Define constants
-int brightness = 20;
+int brightness = 255;
 int power = 1000;
 int count = 0;
 int beat_pre_death = 10;
@@ -22,11 +22,15 @@ int beat_length;
 int breath_length;
 int beats_while_dieing = 5;
 
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+
 unsigned long time;
 
 // Define pins
-int speaker = 8;
-int lights = 6;
+const int lights = 6;
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+
 
 void setup()
 {
@@ -42,7 +46,7 @@ void setup()
 
   myMotor1->setSpeed(0);
   myMotor1->run(FORWARD);
-  
+
   myMotor2->setSpeed(0);
   myMotor2->run(FORWARD);
 
@@ -57,67 +61,43 @@ void setup()
 void beat(int beat_length)
 {
   int phase = time % beat_length;
+  int write_value = 0;
   if (phase < beat_length / 10)
   {
-    noTone(speaker);
-    digitalWrite(lights, 0);
   } else if (phase < 3 * beat_length / 20)
   {
-    tone(speaker, power);
-    analogWrite(lights, brightness);
+    write_value = brightness;
   } else if (phase < 19 * beat_length / 20)
   {
-    noTone(speaker);
-    digitalWrite(lights, 0);
   } else {
-    tone(speaker, power);
-    analogWrite(lights, brightness);
+    write_value = brightness;
   }
+  analogWrite(lights, write_value);
+
 }
 
-void breath(int breath_length)
+void breath(int reading)
 {
-  long phase = time % breath_length;
-  int m_speed;
-  if (phase < 2 * breath_length / 4)
-  {
-    m_speed = 100 * phase / (2 * breath_length / 4);
-  } else
-  {
-    long exhale_time = breath_length - phase;
-    m_speed = 100 * exhale_time / (2 * breath_length / 4);
-  }
-  myMotor->setSpeed(m_speed);
-  myMotor1->setSpeed(m_speed);
-  myMotor2->setSpeed(m_speed);
-  myMotor3->setSpeed(m_speed);
-  Serial.print("Commanding motors to :");
-  Serial.println(m_speed);
+  int motor_speed = map(reading, 0, 5000, 0, 70);
+  Serial.println(motor_speed);
+
+
+  myMotor->setSpeed(motor_speed);
+  myMotor1->setSpeed(motor_speed);
+  myMotor2->setSpeed(motor_speed);
+  myMotor3->setSpeed(motor_speed);
 }
 
 
 void loop() {
-
   time = millis();
-
-  // byte beat_num = 0;
   beat_length = 1000;
-  beat(beat_length);
-  breath_length = 6000;
-  breath(breath_length);
-  // while(beat_num < beat_pre_death + beats_while_dieing)
-  // {
-  //   beat(beat_length);
-  //   beat_num += 1;
-  //   beat_length -= 100;
-  // }
 
-  // Create constant light and sound
-  // tone(speaker, power);
-  // digitalWrite(lights, 0);
-  // delay(3000);
-//  myMotor->run(RELEASE);
-//  myMotor1->run(RELEASE);
-//  myMotor2->run(RELEASE);
-//  myMotor3->run(RELEASE);
+  // read the analog in value:
+  sensorValue = analogRead(analogInPin);
+  // map it to the range of the analog out:
+  outputValue = map(sensorValue, 0, 1023, 0, 5000);
+  beat(beat_length);
+  breath(outputValue);
+  delay(20);
 }
