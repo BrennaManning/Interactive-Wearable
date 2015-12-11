b7941a3048237b375822a652465a69f700bac5f7
David Abrahams <david.abrahams@students.olin.edu>
2015-12-11 00:18:31 -0500

    running averaging and fixed servo output

--- Breathing-Acceleration-Distance/Breathing-Acceleration-Distance.ino
+++ Breathing-Acceleration-Distance/Breathing-Acceleration-Distance.ino
@@ -9,7 +9,6 @@
 // Define constants
 int brightness = 255;
 
-int distance_sensor_value = 0;
 int accelerometer_sensor_value = 0;
 int breathing_sensor_value = 0;
 
@@ -19,8 +18,16 @@ int breathing_leds_out = 0;        // value output to the PWM (analog out)
 
 int accel_threshold = 700; // voltage diff at which to turn LEDs on.
 int distance_threshold = 250; // voltage diff at which to turn LEDs on.
+int distance_max = 3000; // max voltage the distance sensor outputs.
 
 unsigned long time;
+
+const int numDistanceReadings = 5;
+
+int distance_sensor_values[numDistanceReadings];      // the readings from the analog input
+int distanceReadIndex = 0;              // the index of the current reading
+int distanceTotal = 0;                  // the running total
+int distanceAverage = 0;                // the average
 
 // Define output pins (PWM)
 const int servo_out_pin_L = 9;
@@ -39,7 +46,13 @@ void setup()
   Serial.begin(9600);           // set up Serial library at 9600 bps
   servo_L.attach(servo_out_pin_L);
   servo_R.attach(servo_out_pin_R);
+
+  for (int thisReading = 0; thisReading < numDistanceReadings; thisReading++) {
+    distance_sensor_values[thisReading] = 0;
+  }
+
   pinMode(accel_leds_out_pin, OUTPUT);
+  pinMode(breathing_leds_out_pin, OUTPUT);
 }
 
 
@@ -47,20 +60,22 @@ void setup()
 int get_distance_out(int distance_in)
 {
   int output_value = 0;
-  if (distance_in < 250)
+  int updown = 2000;
+  int restTime;
+  if (distance_in < 1000) {restTime = 3000;}
+  else if (distance_in < 1600) {restTime = 2000;}
+  else if (distance_in < 2200) {restTime = 1000;}
+  else {restTime = 0;}
+  if (time % (updown + restTime) < updown)
   {
-    if (time % 5000 < 2000)
-    {
-      int difference = 1000 - abs(time % 5000 - 1000);
-      output_value = map(difference, 0, 1000, 0, 60);
-    }
-    else
-    {
-      int period = map(distance_in, 250, 2000, 1500, 750);
-      int difference = period / 2 - abs(time % period - period / 2);
-      output_value = map(difference, 0, period / 2, 0, 60);
-    }
+    int difference = time % (updown + restTime) - (updown / 2);
+    if (difference < 0) {difference = -difference;} // because abs is fucked
+    difference = updown / 2 - difference;
+    output_value = difference;
+    output_value = map(difference, 0, updown / 2, 0, 60);
   }
+
+  return output_value;
 
 }
 
@@ -105,8 +120,11 @@ void loop() {
 
   time = millis();
 
+
+  distanceTotal = distanceTotal - distance_sensor_values[distanceReadIndex];
+
   // read the analog in value and covert them to millivolts:
-  distance_sensor_value = map(analogRead(distance_in_pin), 0, 1023, 0, 5000);
+  distance_sensor_values[distanceReadIndex] = map(analogRead(distance_in_pin), 0, 1023, 0, 5000);
   accelerometer_sensor_value = map(analogRead(accel_in_pin), 0, 1023, 0, 5000);
   breathing_sensor_value = map(analogRead(breathing_in_pin), 0, 1023, 0, 5000);
 
@@ -114,15 +132,24 @@ void loop() {
   Serial.print(accelerometer_sensor_value);
   Serial.print(". Breathing value: ");
   Serial.print(breathing_sensor_value);
+  Serial.print(". Distance sensor value: ");
+  Serial.print(distanceTotal / numDistanceReadings);
 
-  servo_out = get_distance_out(distance_sensor_value);
+  distanceTotal = distanceTotal + distance_sensor_values[distanceReadIndex];
+
+  // distance_sensor_value = 700;
+
+  servo_out = get_distance_out(distanceTotal / numDistanceReadings);
   accel_leds_out = get_accel_out(accelerometer_sensor_value);
   breathing_leds_out = get_breathing_out(breathing_sensor_value);
 
   set_outputs(servo_out,
               accel_leds_out, breathing_leds_out);
-  Serial.print(". Accel output: ");
-  Serial.println(accel_leds_out);
+  Serial.print(". Servo output: ");
+  Serial.println(servo_out);
+
+  distanceReadIndex = distanceReadIndex + 1;
+  if (distanceReadIndex >= numDistanceReadings) {distanceReadIndex = 0;}
 
   delay(100);
 }
