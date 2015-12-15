a71bbd79b02e6c4a293058b4970ed574a8a3c5e0
David Abrahams <david.abrahams@students.olin.edu>
2015-12-14 22:51:34 -0500

    code update

--- Breathing-Acceleration-Distance/Breathing-Acceleration-Distance.ino
+++ Breathing-Acceleration-Distance/Breathing-Acceleration-Distance.ino
@@ -7,18 +7,22 @@
  int servo_R_pos = 0;
 
 // Define constants
-int brightness = 255;
+int five_v_conversion = 255;
 
 int accelerometer_sensor_value = 0;
 int breathing_sensor_value = 0;
 
-int servo_out = 0;        // value output to the PWM (analog out)
+int servo_L_out = 0;        // value output to the PWM (analog out)
+int servo_R_out = 0;        // value output to the PWM (analog out)
 int accel_leds_out = 0;        // value output to the PWM (analog out)
 int breathing_leds_out = 0;        // value output to the PWM (analog out)
+int breathing_vib_out = 0;        // value output to the PWM (analog out)
 
 int accel_threshold = 700; // voltage diff at which to turn LEDs on.
 int distance_threshold = 250; // voltage diff at which to turn LEDs on.
 int distance_max = 3000; // max voltage the distance sensor outputs.
+
+int accel_led_count = 0;
 
 unsigned long time;
 
@@ -32,6 +36,7 @@ int distanceAverage = 0;                // the average
 // Define output pins (PWM)
 const int servo_out_pin_L = 9;
 const int servo_out_pin_R = 10;
+const int breathing_vib_out_pin = 11;
 const int accel_leds_out_pin = 3;
 const int breathing_leds_out_pin = 5;
 
@@ -39,6 +44,11 @@ const int breathing_leds_out_pin = 5;
 const int distance_in_pin = A1;
 const int accel_in_pin = A4;
 const int breathing_in_pin = A5;
+
+const int servo_L_base = 80;
+const int servo_R_base = 80;
+const int servo_L_max = 50;
+const int servo_R_max = 50;
 
 
 void setup()
@@ -57,9 +67,9 @@ void setup()
 
 
 // takes in current distance sensor in mV and returns the position of the servos.
-int get_distance_out(int distance_in)
+int get_servo_L_out(int distance_in)
 {
-  int output_value = 0;
+  int output_value = servo_L_base;
   int updown = 2000;
   int restTime;
   if (distance_in < 1000) {restTime = 3000;}
@@ -71,10 +81,29 @@ int get_distance_out(int distance_in)
     int difference = time % (updown + restTime) - (updown / 2);
     if (difference < 0) {difference = -difference;} // because abs is fucked
     difference = updown / 2 - difference;
-    output_value = difference;
-    output_value = map(difference, 0, updown / 2, 0, 60);
+    output_value = map(difference, 0, updown / 2, servo_L_base, servo_L_max);
   }
+  return output_value;
 
+}
+
+// takes in current distance sensor in mV and returns the position of the servos.
+int get_servo_R_out(int distance_in)
+{
+  int output_value = servo_R_base;
+  int updown = 2000;
+  int restTime;
+  if (distance_in < 1000) {restTime = 3000;}
+  else if (distance_in < 1600) {restTime = 2000;}
+  else if (distance_in < 2200) {restTime = 1000;}
+  else {restTime = 0;}
+  if (time % (updown + restTime) < updown)
+  {
+    int difference = time % (updown + restTime) - (updown / 2);
+    if (difference < 0) {difference = -difference;} // because abs is fucked
+    difference = updown / 2 - difference;
+    output_value = map(difference, 0, updown / 2, servo_R_base, servo_R_max);
+  }
   return output_value;
 
 }
@@ -87,33 +116,50 @@ int get_accel_out(int accel_in)
   int output_value = 0;
   if (diff > accel_threshold)
   {
-     output_value = 5000;
+    accel_led_count = 100;
   }
 
-  if (time % 1000 < 500)
+  if (accel_led_count > 0)
   {
     output_value = 5000;
-  } else {
-    output_value = 0;
+    accel_led_count = accel_led_count - 1;
   }
+
+  // if (time % 1000 < 500)
+  // {
+  //   output_value = 5000;
+  // } else {
+  //   output_value = 0;
+  // }
 
   return output_value;
 }
 
-int get_breathing_out(int breathing_in)
+int get_breathing_led_out(int breathing_in)
 {
 
-  return breathing_in;
+  return map(breathing_in, 0, 5000, 0, 5000);
 }
 
-void set_outputs(int servo_out, int accel_leds, int breathing_out)
+int get_breathing_vib_out(int breathing_in)
 {
-  analogWrite(accel_leds_out_pin, map(accel_leds, 0, 5000, 0, brightness));
-  servo_R.write(servo_out);
-  servo_L.write(servo_out);
-  analogWrite(breathing_leds_out_pin, map(breathing_out, 0, 5000, 0, brightness));
 
+  int output_value = 0;
 
+  // if (breathing_in > 3500) {output_value = map(breathing_in, 0, 5000, 0, 3300);}
+
+  output_value = map(breathing_in, 0, 5000, 0, 3300);
+
+  return output_value;
+}
+
+void set_outputs(int servo_L_out, int servo_R_out, int accel_leds, int breathing_led_out, int breathing_vib_out)
+{
+  analogWrite(accel_leds_out_pin, map(accel_leds, 0, 5000, 0, five_v_conversion));
+  servo_R.write(servo_R_out);
+  servo_L.write(servo_L_out);
+  analogWrite(breathing_leds_out_pin, map(breathing_led_out, 0, 5000, 0, five_v_conversion));
+  analogWrite(breathing_vib_out_pin, map(breathing_vib_out, 0, 5000, 0, five_v_conversion));
 }
 
 void loop() {
@@ -128,28 +174,34 @@ void loop() {
   accelerometer_sensor_value = map(analogRead(accel_in_pin), 0, 1023, 0, 5000);
   breathing_sensor_value = map(analogRead(breathing_in_pin), 0, 1023, 0, 5000);
 
-  Serial.print("Accelerometer value: ");
-  Serial.print(accelerometer_sensor_value);
-  Serial.print(". Breathing value: ");
-  Serial.print(breathing_sensor_value);
-  Serial.print(". Distance sensor value: ");
-  Serial.print(distanceTotal / numDistanceReadings);
+  distance_sensor_values[distanceReadIndex] = 800;
+
+  // Serial.print("Accelerometer value: ");
+  // Serial.print(accelerometer_sensor_value);
+  // Serial.print(". Breathing value: ");
+  // Serial.print(breathing_sensor_value);
+  // Serial.print(". Distance sensor value: ");
+  // Serial.print(distanceTotal / numDistanceReadings);
 
   distanceTotal = distanceTotal + distance_sensor_values[distanceReadIndex];
 
   // distance_sensor_value = 700;
 
-  servo_out = get_distance_out(distanceTotal / numDistanceReadings);
+  servo_L_out = get_servo_L_out(distanceTotal / numDistanceReadings);
+  servo_R_out = get_servo_R_out(distanceTotal / numDistanceReadings);
   accel_leds_out = get_accel_out(accelerometer_sensor_value);
-  breathing_leds_out = get_breathing_out(breathing_sensor_value);
+  breathing_leds_out = get_breathing_led_out(breathing_sensor_value);
+  breathing_vib_out = get_breathing_vib_out(breathing_sensor_value);
 
-  set_outputs(servo_out,
-              accel_leds_out, breathing_leds_out);
+  set_outputs(servo_L_out, servo_R_out,
+              accel_leds_out, breathing_leds_out, breathing_vib_out);
+  Serial.print(". Breathing vib output: ");
+  Serial.print(breathing_vib_out);
   Serial.print(". Servo output: ");
-  Serial.println(servo_out);
+  Serial.println(servo_L_out);
 
   distanceReadIndex = distanceReadIndex + 1;
   if (distanceReadIndex >= numDistanceReadings) {distanceReadIndex = 0;}
 
-  delay(100);
+  delay(5);
 }
